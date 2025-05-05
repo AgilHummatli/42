5782ac5723fc97939795b23ed2632505384b0406
AgilHummatli <hummatli.agil@gmail.com>
2025-05-05 11:00:00 +0400

    feat: C03 ex02 ex03 ft_strcat and ft_strncat

---
+++ 42_school/C03/ex02/ft_strcat.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahummatl <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 21:36:11 by ahummatl          #+#    #+#             */
+/*   Updated: 2024/10/16 21:52:38 by ahummatl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	dest_size;
+
+	i = 0;
+	dest_size = 0;
+	while (dest[dest_size] != '\0')
+		dest_size++;
+	while (src[i] != '\0')
+	{
+		dest[dest_size] = src[i];
+		dest_size++;
+		i++;
+	}
+	dest[dest_size] = '\0';
+	return (dest);
+}
